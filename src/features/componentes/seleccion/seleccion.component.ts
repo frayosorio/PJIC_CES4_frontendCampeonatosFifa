86c2c6e12bd5a8d861b500ec8c9a2e3764d2ce62
fray osorio <frayosorio@gmail.com>
2025-09-15 07:06:18 -0500

    Se agregan primeros componentes y se definen sus rutas

---
+++ src/features/componentes/seleccion/seleccion.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+
+@Component({
+  selector: 'app-seleccion',
+  imports: [
+    ReferenciasMaterialModule
+  ],
+  templateUrl: './seleccion.component.html',
+  styleUrl: './seleccion.component.css'
+})
+export class SeleccionComponent {
+
+}
