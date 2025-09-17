a7eb3427a3a0ce8c78dfbb1f2cd095feeb1c65c1
fray osorio <frayosorio@gmail.com>
2025-09-17 07:35:07 -0500

    Se agrega modelo y servicio para SELECCIONES

---
+++ src/core/servicios/seleccion.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Seleccion } from '../../shared/entidades/seleccion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeleccionService {
+
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = `${environment.urlBase}selecciones/`;
+  }
+
+  public listar(): Observable<Seleccion[]> {
+    return this.http.get<Seleccion[]>(`${this.url}listar`);
+  }
+
+}
--- src/features/componentes/seleccion/seleccion.component.ts
+++ src/features/componentes/seleccion/seleccion.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
 
 @Component({
   selector: 'app-seleccion',
+  //standalone: true,
   imports: [
-    ReferenciasMaterialModule
+    ReferenciasMaterialModule,
+    NgxDatatableModule
   ],
   templateUrl: './seleccion.component.html',
   styleUrl: './seleccion.component.css'
 })
 export class SeleccionComponent {
 
+
+  buscar() {
+
+  }
+
+  agregar() {
+
+  }
+
+  modificar() {
+
+  }
+
+  eliminar() {
+
+  }
 }
