8f524013ae16c830ac75fbd4361996d1a638ffe1
fray osorio <frayosorio@gmail.com>
2025-11-10 07:04:25 -0500

    Se agrega funcionalidad para Protección de rutas mediante GUARDAS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from '../features/componentes/inicio/inicio.component';
 import { SeleccionComponent } from '../features/componentes/seleccion/seleccion.component';
 import { CampeonatoComponent } from '../features/componentes/campeonato/campeonato.component';
+import { AutorizacionGuard } from '../core/guardas/autorizacion.guard';
 
 export const RUTA_DEFAULT = "/inicio";
 
 export const routes: Routes = [
     { path: "", redirectTo: "inicio", pathMatch: "full" },
     { path: "inicio", component: InicioComponent },
-    { path: "selecciones", component: SeleccionComponent },
-    { path: "campeonatos", component: CampeonatoComponent }
+    { path: "selecciones", component: SeleccionComponent, canActivate: [AutorizacionGuard] },
+    { path: "campeonatos", component: CampeonatoComponent, canActivate: [AutorizacionGuard] }
 ];
